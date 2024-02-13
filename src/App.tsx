020c5f1d6f522310299b743994081ff283dcf3cb
Igor Pavlovic <ip@igorpavlovic.com>
2024-02-13 14:48:07 +0100

    Removing imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import React, { useEffect } from "react";
-import logo from "./logo.svg";
-import * as Three from "three";
 import "./App.css";
-import { getFormaToken } from "./store/oauth";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import CallbackPage from "./components/CallbackPage";
 import { Forma } from "forma-embedded-view-sdk/auto";
 import { createSubscription } from "./store/subscription";
 import Scene from "./Scene"
