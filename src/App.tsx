6f70743005ac0c2cc9700a395e894eeda914f885
Igor Pavlovic <ip@igorpavlovic.com>
2024-02-13 21:08:55 +0100

    TS fixes

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,12 @@ import Scene from "./components/Scene";
 import { useGlobalStore } from "./store/global";
 import * as THREE from "three";
 import { Forma } from "forma-embedded-view-sdk/auto";
-import { Vec3 } from "forma-embedded-view-sdk/dist/internal/scene/design-tool";
-
 
 function App() {
   const store = useGlobalStore();
-  const [from, setFrom] = useState<Vec3>();
-  const [to, setTo] = useState<Vec3>();
+  const dummyVector = new THREE.Vector3();
+  const [from, setFrom] = useState<THREE.Vector3>(dummyVector);
+  const [to, setTo] = useState<THREE.Vector3>(dummyVector);
 
   useEffect(() => {
     // Fetch all paths to buildings in the current proposal.
@@ -19,14 +18,13 @@ function App() {
     //   console.log('terrain ', paths)
     // });
 
-
+    store.getAllGeometry()
     store.createSubscription();
     store.createCameraSubscription();
-    store.getAllGeometry()
   });
 
   const intersect = () => {
-    store.checkIntersection(from as THREE.Vector3, to as THREE.Vector3)
+    store.checkIntersection(from, to)
   }
 
   const pickFrom = () => {
@@ -46,9 +44,9 @@ function App() {
   return (
     <div className="app" >
       <div style={{ display: 'flex', gap: '10px' }}>
-      <button onClick={() => pickFrom()}>From</button>
-      <button onClick={() => pickTo()}>To</button>
-      <button onClick={() => intersect()}>Intersect</button>
+        <button onClick={() => pickFrom()}>From</button>
+        <button onClick={() => pickTo()}>To</button>
+        <button onClick={() => intersect()}>Intersect</button>
       </div>
       <Scene />
     </div>
