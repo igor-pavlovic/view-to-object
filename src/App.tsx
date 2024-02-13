f95e944d6ded5ea69087a46318e410b852f6a07d
Igor Pavlovic <ip@igorpavlovic.com>
2024-02-13 14:45:48 +0100

    Adding scene

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from "react";
-import logo from "./logo.svg";
-import * as Three from "three";
+import React, { useEffect } from "react";
 import "./App.css";
+import { Forma } from "forma-embedded-view-sdk/auto";
+import Scene from "./Scene";
 
 function App() {
+  // useEffect(() => {
+  //   Forma.geometry
+  //     .getPathsByCategory({ category: "buildings" })
+  //     .then((paths) => { console.log(paths.length) })
+  // }, [])
+
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Scene />
     </div>
   );
 }
